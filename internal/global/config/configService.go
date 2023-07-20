1d1be756057fcb55a4330e44ebefd43017189df6
fangnan700 <fangnan0122@outlook.com>
2023-07-20 16:45:07 +0800

    新增数据库功能模块

--- internal/global/config/configService.go
+++ internal/global/config/configService.go
@@ -14,11 +14,13 @@ var (
 )
 
 func init() {
+	// 读取配置文件
 	buff, err = os.ReadFile("config.yaml")
 	if err != nil {
 		fmt.Println(err)
 	}
 
+	// 解析配置到结构体
 	err = yaml.Unmarshal(buff, &GlobalConfig)
 	if err != nil {
 		fmt.Println(err)
--- internal/global/mysql/mysqlService.go
+++ internal/global/mysql/mysqlService.go
@@ -2,18 +2,19 @@ package mysql
 
 import (
 	"CTFe/internal/global/config"
+	"CTFe/internal/models"
 	"database/sql"
 	"fmt"
 	_ "github.com/go-sql-driver/mysql"
-	"os/user"
 )
 
 var (
-	err error
-	db  *sql.DB
+	db *sql.DB
 )
 
 func init() {
+	var err error
+
 	info := fmt.Sprintf(
 		"%s:%s@tcp(%s:%d)/%s",
 		config.GlobalConfig.MySqlConfig.Username,
@@ -22,16 +23,83 @@ func init() {
 		config.GlobalConfig.MySqlConfig.Port,
 		config.GlobalConfig.MySqlConfig.Database,
 	)
+
 	db, err = sql.Open("mysql", info)
 	if err != nil {
 		fmt.Println(err)
 	}
+
 	err = db.Ping()
 	if err != nil {
 		fmt.Println(err)
 	}
 }
 
-func InsertUser(user user.User) {
+/*
+	用户表相关操作
+*/
 
+// InsertUser 新增用户
+func InsertUser(user models.Users) error {
+	var (
+		err  error
+		tx   *sql.Tx
+		stmt *sql.Stmt
+	)
+
+	// 开启事务
+	tx, err = db.Begin()
+	if err != nil {
+		return err
+	}
+
+	//准备sql语句
+	stmt, err = tx.Prepare("INSERT INTO users (user_id, user_name, user_sex, email, phone, school, student_num, create_time) VALUES (?, ?, ?, ?, ?, ?, ?, ?);")
+	if err != nil {
+		return err
+	}
+
+	//设置参数以及执行sql语句
+	_, err = stmt.Exec(user.UserId, user.UserName, user.UserSex, user.Email, user.Phone, user.School, user.StudentNum, user.CreateTime)
+	if err != nil {
+		return err
+	}
+	err = tx.Commit()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// SelectLastUserId 查询最后一个用户ID
+func SelectLastUserId() (int, error) {
+	var (
+		err        error
+		row        *sql.Row
+		lastUserId int
+	)
+
+	row = db.QueryRow("SELECT user_id FROM users ORDER BY user_id LIMIT 1;")
+	err = row.Scan(&lastUserId)
+	if err != nil {
+		return -1, err
+	}
+	return lastUserId, nil
+}
+
+// SelectUserById 根据用户ID查询用户
+func SelectUserById(userId int) (models.Users, error) {
+	var (
+		err  error
+		row  *sql.Row
+		user models.Users
+	)
+
+	row = db.QueryRow("SELECT * FROM users WHERE user_id = ?;", userId)
+	err = row.Scan(&user.UserId, &user.UserName, &user.UserSex, &user.Email, &user.Phone, &user.School, &user.StudentNum, &user.CreateTime)
+	if err != nil {
+		return user, err
+	}
+
+	return user, nil
 }
